2064475f607e7bf545c0bdf79e3ce7af4e45fe5b
吳 東穎 <dct55660@gmail.com>
2020-10-25 23:38:26 +0800

    V2

--- WebApplication20201025/Controllers/CustomerController.cs
+++ WebApplication20201025/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,124 @@ namespace WebApplication20201025.Controllers
         {
             IQueryable<Customer> cusData = from q in db.Customers
                           select q;
+            
             return View(cusData);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer cus)
+        {
+            if(cus != null)
+            {
+                db.Customers.Add(cus);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        public ActionResult Edit(string ID)
+        {
+            if (!string.IsNullOrEmpty(ID))
+            {
+                var cusData = (from q in db.Customers
+                               where q.CustomerID == ID
+                               select q).FirstOrDefault();
+                return View(cusData);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Customer cus)
+        {
+            if (cus != null)
+            {
+                var cusData = (from q in db.Customers
+                               where q.CustomerID == cus.CustomerID
+                               select q).FirstOrDefault();
+
+                if (cusData != null)
+                {
+                    cusData.CompanyName = cus.CompanyName;
+                    cusData.ContactName = cus.ContactName;
+                    cusData.ContactTitle = cus.ContactTitle;
+                    cusData.Address = cus.Address;
+                    cusData.City = cus.City;
+                    cusData.Phone = cus.Phone;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public string Delete(string ID)
+        {
+            string MSG = "";
+            List<dynamic> jList = new List<dynamic>();
+            if (!string.IsNullOrEmpty(ID))
+            {                
+                try
+                {
+                    var cusData = (from q in db.Customers
+                                   where q.CustomerID == ID
+                                   select q).FirstOrDefault();
+                    if(cusData!= null)
+                    {
+                        db.Customers.Remove(cusData);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        MSG = JsonConvert.SerializeObject("NoData");
+                    }
+                }
+                catch
+                {
+                    MSG = JsonConvert.SerializeObject("Fail");
+                }
+                if(MSG == JsonConvert.SerializeObject("Fail"))
+                {
+                    return MSG;
+                }
+                else
+                {
+                    var cusData = from q in db.Customers
+                                  select q;
+                    foreach(var item in cusData)
+                    {
+                        jList.Add(item);
+                    }
+                    string jsCusData = JsonConvert.SerializeObject(jList);
+                    return jsCusData;
+                }
+            }
+            else
+            {
+                MSG = JsonConvert.SerializeObject("NoData");
+                return MSG;
+            }
         }
     }
 }
---
+++ WebApplication20201025/Models/CustomerClass.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication20201025.Models
+{
+    [MetadataType(typeof(CustomerMetadata))]
+    public partial class Customer
+    {
+        private class CustomerMetadata
+        {
+            [JsonIgnore]
+            public virtual ICollection<Order> Orders { get; set; }
+
+            [JsonIgnore]
+            public virtual ICollection<CustomerDemographic> CustomerDemographics { get; set; }
+        }
+    }
+
+    [MetadataType(typeof(OrderMetadata))]
+    public partial class Order
+    {
+        private class OrderMetadata
+        {
+            [JsonIgnore]
+            public virtual Customer Customer { get; set; }
+
+            [JsonIgnore]
+            public virtual CustomerDemographic CustomerDemographics { get; set; }
+
+            [JsonIgnore]
+            public virtual Employee Employee { get; set; }
+
+            [JsonIgnore]
+            public virtual ICollection<Order_Detail> Order_Details { get; set; }
+
+            [JsonIgnore]
+            public virtual Shipper Shipper { get; set; }
+        }
+    }
+
+    [MetadataType(typeof(CustomerDemographicMetadata))]
+    public partial class CustomerDemographic
+    {
+        private class CustomerDemographicMetadata
+        {
+            [JsonIgnore]
+            public virtual ICollection<Customer> Customers { get; set; }
+        }
+    }
+}

d537eddbad4af96f33d30c85d59c23b484524256
吳 東穎 <dct55660@gmail.com>
2020-10-25 15:44:14 +0800

    加入專案檔案。

---
+++ WebApplication20201025/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication20201025.Models;
+
+namespace WebApplication20201025.Controllers
+{
+    public class CustomerController : Controller
+    {
+        NorthwindEntities1 db = new NorthwindEntities1();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            IQueryable<Customer> cusData = from q in db.Customers
+                          select q;
+            return View(cusData);
+        }
+    }
+}
